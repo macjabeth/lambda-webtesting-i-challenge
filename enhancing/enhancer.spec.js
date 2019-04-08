a362e5fe2717e8f3c105a9b1e83c8b3b23687fe7
macjabeth <macbethjonathan@gmail.com>
2019-04-08 14:41:52 -0400

    install necessary dependencies & implement `repair` unit testing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,12 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+test('repair restores the durability to 100', () => {
+  const expected = 100;
+  const repair = enhancer.repair({
+    name: 'durability test',
+    durability: 10,
+    enhancement: 5
+  });
+
+  expect(repair.durability).toBe(expected);
+});
